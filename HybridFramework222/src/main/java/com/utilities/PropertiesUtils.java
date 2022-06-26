f43a82f932632fbdbf9f0745efee36b515e344ce
mangesh1404 <mangeshvyahalkar@gmail.com>
2022-06-26 17:59:17 +0530

    hybrid framework

---
+++ HybridFramework222/src/main/java/com/utilities/PropertiesUtils.java
@@ -0,0 +1,22 @@
+package com.utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertiesUtils {
+
+	public static Properties prop=null;
+	public static FileInputStream fis=null;
+	
+	public static String readProperty(String key)throws Exception {
+		prop= new Properties();
+		fis=new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/config.properties");
+		prop.load(fis);
+		return prop.getProperty(key);
+	
+	}
+	
+	public static void main(String[] args) throws Exception{
+		System.out.println(readProperty("browser"));
+	}
+}
